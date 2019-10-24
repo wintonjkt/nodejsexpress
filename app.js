3bd6c11065c1c70c3cab7bfc72713ee485b82e0b
wintonjkt <wintonjkt@gmail.com>
2019-10-24 02:54:49 -0700

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello HCI Team, good evening!");
+  res.send("Hello Hybrid Cloud Integration, good evening!");
 });
  
 module.exports.app = app;
