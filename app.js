5681e51fb676487e15a195b547456c8007558a92
wintonjkt <wintonjkt@gmail.com>
2019-10-25 21:17:43 -0700

    commit for meetup event

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Hybrid Cloud Integration, good evening!");
+  res.send("Hello Indonesian Developers, good evening!");
 });
  
 module.exports.app = app;
