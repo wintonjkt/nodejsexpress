2c6567a96c66c0db727a92be33a3b2650324167b
wintonjkt <wintonjkt@gmail.com>
2019-10-27 23:56:28 -0700

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Jakarta, Selamat Pagi!");
+  res.send("Hello Mas Panji, Selamat Pagi!");
 });
  
 module.exports.app = app;
