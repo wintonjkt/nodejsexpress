c431cc042878e0467f0e26cb6a1429471354de5b
wintonjkt <wintonjkt@gmail.com>
2019-10-31 05:17:16 -0700

    test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Pak Agus, Selamat Malam!");
+  res.send("Hello Pak Agus, Selamat Malam, terima kasih!");
 });
  
 module.exports.app = app;
