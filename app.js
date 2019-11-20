5fbb54012ab175fb2328b7f0e9ca1f5c24a7f6bc
wintonjkt <wintonjkt@gmail.com>
2019-11-20 03:07:52 -0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello This is a Build Demo!");
+  res.send("Hello This is a Build Demo for event!");
 });
  
 module.exports.app = app;
