5eeca0dbe5541efa9551b60adb6fed0b172ac3fb
wintonjkt <wintonjkt@gmail.com>
2019-11-20 00:49:40 -0800

    build demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Pak Agus, Selamat Malam, terima kasih!");
+  res.send("Hello This is a Build Demo!");
 });
  
 module.exports.app = app;
