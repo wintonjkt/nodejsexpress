23a02b2a9c62c47606412fde46c6a7ff8ff5d23c
wintonjkt <wintonjkt@gmail.com>
2019-11-20 22:08:12 -0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello This is a Build Demo for J2C event!");
+  res.send("Hello This is a Build Demo for CloudPak event!");
 });
  
 module.exports.app = app;
