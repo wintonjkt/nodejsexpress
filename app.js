7a6165ab9866f6a5a7ffb1f0e22823abd918d758
wintonjkt <wintonjkt@gmail.com>
2020-02-09 22:40:32 -0800

    message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello this is Cloud Pak Application event!");
+  res.send("Hello this is Cloud Pak Automation Demo!");
 });
  
 module.exports.app = app;
