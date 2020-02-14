dcf7a771b712c482573bf7ede28fa36c4c382aa1
wintonjkt <wintonjkt@gmail.com>
2020-02-13 18:12:30 -0800

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello this is Cloud Pak Automation Demonstration Testing 123");
+  res.send("Hello this is Cloud Pak Automation Demonstration Testing 1234");
 });
  
 module.exports.app = app;
